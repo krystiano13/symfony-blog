27c917fdacd1f3ec98f6ecc575d811fbd15b6ed1
krystiano13 <crystianotv@gmail.com>
2024-03-05 10:01:23 +0100

    implement showing about section on phone

--- assets/about.js
+++ assets/about.js
@@ -1,1 +1,16 @@
 const aboutSection = document.querySelector('#about');
+const hamburgerButton = document.querySelector('#hamburger');
+const aboutClass = "transition flex gap-5 flex-col justify-start items-center p-4 w-72 bg-gray-50 h-[100vh] xl:translate-x-0 fixed z-50";
+
+let hidden = true;
+
+hamburgerButton.addEventListener('click', () => {
+    hidden = !hidden;
+
+    if(hidden) {
+        aboutSection.className = `${aboutClass} translate-x-[-100vw]`;
+    }
+    else {
+        aboutSection.className = `${aboutClass}`;
+    }
+});
