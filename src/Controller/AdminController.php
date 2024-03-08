0d4b9854cee944478ab10e5c6f0551911e54e8ae
krystiano13 <crystianotv@gmail.com>
2024-03-08 13:34:56 +0100

    implement deleting posts using admin dashboard

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,14 +2,16 @@
 
 namespace App\Controller;
 
+use App\Repository\CommentRepository;
 use App\Repository\PostRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class AdminController extends AbstractController
 {
-    #[Route('/admin', name: 'app_admin')]
+    #[Route('/admin', name: 'app_admin', methods: ['GET'])]
     public function index(PostRepository $postRepository): Response
     {
         $posts = $postRepository -> findAll();
@@ -18,5 +20,23 @@ class AdminController extends AbstractController
             'controller_name' => 'AdminController',
             'posts' => $posts
         ]);
+    }
+
+    #[Route('/admin/post/delete/{id}', name: 'app_admin_post_delete', methods: ['DELETE', 'GET'])]
+    public function destroy(int $id, PostRepository $pr, EntityManagerInterface $em, CommentRepository $cr):Response {
+        $post = $pr -> find($id);
+
+        if($post -> getId() !== null) {
+            $comments = $cr -> findBy(['post' => $post]);
+            
+            foreach($comments as $comment) {
+                $post -> removeComment($comment);
+            }
+
+            $em -> remove($post);
+            $em -> flush();
+        }
+
+        return $this->redirectToRoute('app_admin');
     }
 }
