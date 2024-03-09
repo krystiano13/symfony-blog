2d14c696f314996a5b93aa3a6989cf4bab842bc1
krystiano13 <crystianotv@gmail.com>
2024-03-09 13:27:19 +0100

    make uploaded image visible on blog card

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
 use App\Repository\CommentRepository;
 use App\Repository\PostRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -10,6 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Validator\Constraints\Date;
 
 class AdminController extends AbstractController
 {
@@ -30,22 +32,33 @@ class AdminController extends AbstractController
     }
 
     #[Route('/admin/addpost', name: 'app_admin_store', methods: ['POST', 'GET'])]
-    public function store(Request $request) {
+    public function store(Request $request, EntityManagerInterface $em) {
         $title = $request -> get('title');
         $description = $request -> get('description');
+
+        $post = new Post();
+        $post -> setTitle($title);
+        $post -> setDescription($description);
+
+        $date = new \DateTime('now');
+
+        $post -> setDate($date -> format('d M Y'));
 
         /**
          * Uploaded Image
          * @type UploadedFile
          */
         $image_file = $request -> files -> get('image_file');
+        $path = $this -> getParameter('kernel.project_dir')."/public/build/images/";
+        $name = uniqid().$image_file -> getClientOriginalName();
 
-        $image_file -> move(
-            $this -> getParameter('kernel.project_dir')."/public/build/images/",
-            uniqid().$image_file -> getClientOriginalName()
-        );
+        $image_file -> move($path,$name);
 
-        dd($image_file);
+        $post -> setImage("build/images/".$name);
+        $em -> persist($post);
+        $em -> flush();
+
+        return $this->redirectToRoute('app_admin');
     }
 
     #[Route('/admin/post/delete/{id}', name: 'app_admin_post_delete', methods: ['DELETE', 'GET'])]
